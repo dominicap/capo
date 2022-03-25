d6cf500aca2fc4c1c046560b90fe64dacdc0b800
Dominic Philip <domi.a.philip@gmail.com>
2022-03-25 16:37:53 -0400

    Initial commit

---
+++ Capo Watch Extension/CapoApp.swift
@@ -0,0 +1,21 @@
+//
+//  CapoApp.swift
+//  Capo Watch Extension
+//
+//  Created by Dominic Philip on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CapoApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
---
+++ Capo/CapoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapoApp.swift
+//  Capo
+//
+//  Created by Dominic Philip on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CapoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
