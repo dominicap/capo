90f797c0089a32fc91863a242c61895c937e86ae
Dominic Philip <domi.a.philip@gmail.com>
2022-04-01 02:42:18 -0400

    Format project

--- Capo/CapoApp.swift
+++ Capo/CapoApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct CapoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
